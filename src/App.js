e8539cbed919b7a3b8df99fcad482e8e0fc7dae0
Hy-M <humayraa.m@outlook.com>
2020-02-10 14:45:18 +0000

    Creating footer component

--- src/App.js
+++ src/App.js
@@ -1,15 +1,27 @@
-import React from 'react';
+import React, { Component } from 'react';
+import { Router } from '@reach/router';
 import './App.css';
 import Title from './components/Title';
 import Nav from './components/Nav';
+import Footer from './components/Footer';
 
-function App() {
-  return (
-    <div className="App">
-      <Title />
-      <Nav />
-    </div>
-  );
+class App extends Component {
+  state = {
+    user: 'jessjelly'
+  }
+
+  render() {
+    return (
+      <div className="App">
+       <Title user={this.state.user} /> 
+       <Nav />
+       <Router>
+         
+       </Router>
+       <Footer />
+      </div>
+    );
+  }
 }
 
 export default App;
--- src/components/Title.jsx
+++ src/components/Title.jsx
@@ -1,11 +1,14 @@
 import React from 'react';
 
-const Title = () => {
+const Title = ({ user }) => {
     return (
-        <header className="header">
-            <h1 className="header--title">Northcoders News</h1>
-            <img className="header--logo" src='https://pbs.twimg.com/media/Dc6Z6GbWkAELnoS.png' alt="NC logo"></img>
-        </header>
+            <div>
+                <header className="header">
+                    <h1 className="header--title">NC News</h1>
+                    {/* <img className="header--logo" src='https://pbs.twimg.com/media/Dc6Z6GbWkAELnoS.png' alt="NC logo"></img> */}
+                    <p className="header--username">{user} is logged in!</p>
+                </header>
+            </div>
     );
 };
 

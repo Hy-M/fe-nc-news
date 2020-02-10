e69722c17855f54190bf7501620a6df9d0fdc841
Hy-M <humayraa.m@outlook.com>
2020-02-10 14:15:01 +0000

    Creating title component

---
+++ src/components/Title.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Title = () => {
+    return (
+        <header className="header">
+            <h1 className="header--title">Northcoders News</h1>
+            <img className="header--logo" src='https://pbs.twimg.com/media/Dc6Z6GbWkAELnoS.png' alt="NC logo"></img>
+        </header>
+    );
+};
+
+export default Title;

4681541926023a2f2e04807620c67b162ce82279
Hy-M <humayraa.m@outlook.com>
2020-02-13 12:26:08 +0000

    Disabling voter buttons on click, refactoring

--- src/components/Voter.jsx
+++ src/components/Voter.jsx
@@ -1,57 +1,125 @@
-import React, { Component } from 'react';
-import * as api from '../utils/api';
+import React, { Component } from "react";
+import * as api from "../utils/api";
 
 class Voter extends Component {
-    state = {
-        optimisticVote: this.props.votes,
-        userVote: 0,
-        btnHasBeenClicked: false,
-        err: false
-    }
+  state = {
+    optimisticVote: 0,
+    err: false
+  };
 
-    handleVoteClick = (userVote) => {             
-        this.setState({ userVote, btnHasBeenClicked: true}, () => {
-            this.updateVotes()
-        })
+  handleVoteClick = userVote => {
+    if (this.props.article_id) {
+      api.patchVotes("articles", this.props.article_id, userVote).catch(() => {
+        this.setState({ err: true });
+      });
+    } else {
+      api.patchVotes("comments", this.props.comment_id, userVote).catch(() => {
+        this.setState({ err: true });
+      });
     }
+    this.setState(currentState => {
+      return {
+        optimisticVote: currentState.optimisticVote + userVote
+      };
+    });
+  };
 
-    updateVotes = () => {
-        let inc_votes = {
-            inc_votes: this.state.userVote
-        }
-
-        if (this.props.article_id) {
-            api.patchVotes('articles', this.props.article_id, inc_votes).catch(() => {
-                this.setState({ err: true })
-            })
-        } else {
-            api.patchVotes('comments', this.props.comment_id, inc_votes).catch(() => {
-                this.setState({ err: true })
-            })
-        }
-        
-        this.setState((currentState) => {
-            return { optimisticVote: currentState.optimisticVote + inc_votes.inc_votes}
-        })
-    }
-
-    render() {
-        const { optimisticVote, btnHasBeenClicked, err } = this.state;
-        return (
-            <section className="voter">
-                <p className="voter--title">Votes: {optimisticVote}</p>
-                <button className={btnHasBeenClicked ? "voter--btn-inactive voter--btn" : "voter--btn"} id="upvote" onClick={((clickEvent) => {
-                    clickEvent.preventDefault();
-                    this.handleVoteClick(1)
-                })}><i className="far fa-thumbs-up"></i></button>
-                <button className={btnHasBeenClicked ? "voter--btn-inactive voter--btn" : "voter--btn"} id="downvote" onClick={((clickEvent) => {
-                    clickEvent.preventDefault();
-                    this.handleVoteClick(-1)
-                })}><i className="far fa-thumbs-down"></i></button>
-                { btnHasBeenClicked && err && <p className="voter--errorMsg">Sorry, votes can't be added right now.</p>}    
-            </section>
-        );
-    }
+  render() {
+    const { optimisticVote, btnHasBeenClicked, err } = this.state;
+    return (
+      <section className="voter">
+        <p className="voter--title">
+          Votes: {optimisticVote + this.props.votes}
+        </p>
+        <button
+          className={
+            btnHasBeenClicked ? "voter--btn-inactive voter--btn" : "voter--btn"
+          }
+          id="upvote"
+          onClick={clickEvent => {
+            this.handleVoteClick(1);
+          }}
+          disabled={this.state.optimisticVote > 0}
+        >
+          <i className="far fa-thumbs-up"></i>
+        </button>
+        <button
+          className={
+            btnHasBeenClicked ? "voter--btn-inactive voter--btn" : "voter--btn"
+          }
+          id="downvote"
+          onClick={clickEvent => {
+            this.handleVoteClick(-1);
+          }}
+          disabled={this.state.optimisticVote < 0}
+        >
+          <i className="far fa-thumbs-down"></i>
+        </button>
+        {btnHasBeenClicked && err && (
+          <p className="voter--errorMsg">
+            Sorry, votes can't be added right now.
+          </p>
+        )}
+      </section>
+    );
+  }
 }
 
 export default Voter;
+// import React, { Component } from 'react';
+// import * as api from '../utils/api';
+
+// class Voter extends Component {
+//     state = {
+//         optimisticVote: this.props.votes,
+//         userVote: 0,
+//         btnHasBeenClicked: false,
+//         err: false
+//     }
+
+//     handleVoteClick = (userVote) => {             
+//         this.setState({ userVote, btnHasBeenClicked: true}, () => {
+//             this.updateVotes()
+//         })
+//     }
+
+//     updateVotes = () => {
+//         let inc_votes = {
+//             inc_votes: this.state.userVote
+//         }
+
+//         if (this.props.article_id) {
+//             api.patchVotes('articles', this.props.article_id, inc_votes).catch(() => {
+//                 this.setState({ err: true })
+//             })
+//         } else {
+//             api.patchVotes('comments', this.props.comment_id, inc_votes).catch(() => {
+//                 this.setState({ err: true })
+//             })
+//         }
+        
+//         this.setState((currentState) => {
+//             return { optimisticVote: currentState.optimisticVote + inc_votes.inc_votes}
+//         })
+//     }
+
+//     render() {
+//         const { optimisticVote, btnHasBeenClicked, err } = this.state;
+//         return (
+//             <section className="voter">
+//                 <p className="voter--title">Votes: {optimisticVote}</p>
+//                 <button className={btnHasBeenClicked ? "voter--btn-inactive voter--btn" : "voter--btn"} id="upvote" onClick={((clickEvent) => {
+//                     clickEvent.preventDefault();
+//                     this.handleVoteClick(1)
+//                 })}><i className="far fa-thumbs-up"></i></button>
+//                 <button className={btnHasBeenClicked ? "voter--btn-inactive voter--btn" : "voter--btn"} id="downvote" onClick={((clickEvent) => {
+//                     clickEvent.preventDefault();
+//                     this.handleVoteClick(-1)
+//                 })}><i className="far fa-thumbs-down"></i></button>
+//                 { btnHasBeenClicked && err && <p className="voter--errorMsg">Sorry, votes can't be added right now.</p>}    
+//             </section>
+//         );
+//     }
+// }
+
+// export default Voter;
